723d18b9b372e526cd42cbbd6fa2c1262120ffef
Hit-124 <khsur50011@gmail.com>
2025-11-17 20:49:11 +0200

    Delete support.js to refresh Git state

--- routes/support.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/test", (req, res) => {
-  res.json({ status: "ok", message: "Support route works" });
-});
-
-export default router;
