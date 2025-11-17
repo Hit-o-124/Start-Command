eca59d60763b8f33ce55ebf53f486981f8fdc152
Hit-124 <khsur50011@gmail.com>
2025-11-17 20:40:35 +0200

    Fix analytics router (final working version)

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,2 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+  res.json({ status: "ok", message: "Analytics route works" });
+});
 
 export default router;
